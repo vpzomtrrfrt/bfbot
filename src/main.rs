61961d273cf86572ea964b18e4fc489685cbca7b
Colin Reeder <colin@vpzom.click>
2020-06-13 10:08:55 -0600

    Adopt noob

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,77 @@
-extern crate heliometer;
-extern crate serenity;
+use futures::TryStreamExt;
 
 #[derive(Debug)]
 enum Error {
     BFError(heliometer::Error),
     ParseError(std::string::FromUtf8Error),
+    JoinError(tokio::task::JoinError),
 }
 
-struct Handler;
-
-impl serenity::client::EventHandler for Handler {
-    fn message(&self, ctx: serenity::client::Context, msg: serenity::model::channel::Message) {
-        if msg.content.starts_with("%bf ") {
-            let program = &msg.content[4..];
-            let mut output = Vec::new();
-            let mut input: &[u8] = &[];
-            let result = heliometer::execute(program, &mut input, &mut output)
-                .map_err(|e| Error::BFError(e));
-            let output = String::from_utf8(output).map_err(|e| Error::ParseError(e));
-            match match result.and(output) {
-                Ok(ref output) => msg.reply(&ctx, &output),
-                Err(err) => msg.reply(&ctx, &format!("Error: {:?}", err)),
-            } {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("Failed something: {}", e);
-                }
-            }
-        }
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), noob::Error> {
     let token = std::env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN");
 
-    let mut client = serenity::Client::new(&token, Handler).expect("Error creating client");
+    let (client, stream) = noob::Client::connect(&token).await?;
+    let client = std::sync::Arc::new(client);
+    stream
+        .try_for_each(|evt| {
+            if let noob::Event::MessageCreate(msg) = evt {
+                let content = msg.content;
+                let author = msg.author;
+                let channel_id = msg.channel_id;
+                if content.starts_with("%bf ") {
+                    let client = client.clone();
+                    tokio::spawn(async move {
+                        let result = flatten_result(
+                            tokio::task::spawn_blocking(move || {
+                                let program = &content[4..];
 
-    client.start().unwrap();
+                                let mut output = Vec::new();
+                                let mut input: &[u8] = &[];
+
+                                heliometer::execute(program, &mut input, &mut output)
+                                    .map_err(|e| Error::BFError(e))
+                                    .and(
+                                        String::from_utf8(output).map_err(|e| Error::ParseError(e)),
+                                    )
+                            })
+                            .await
+                            .map_err(Error::JoinError),
+                        );
+
+                        let res = client
+                            .send_message(
+                                &noob::MessageBuilder::new(&format!(
+                                    "<@{}>: {}",
+                                    author.id,
+                                    match result {
+                                        Ok(output) => output,
+                                        Err(err) => format!("Error: {:?}", err),
+                                    },
+                                )),
+                                &channel_id,
+                            )
+                            .await;
+
+                        if let Err(e) = res {
+                            eprintln!("Failed something {:?}", e);
+                        }
+                    });
+                }
+            }
+
+            futures::future::ready(Ok(()))
+        })
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+fn flatten_result<T, E>(res: Result<Result<T, E>, E>) -> Result<T, E> {
+    match res {
+        Err(e) => Err(e),
+        Ok(Err(e)) => Err(e),
+        Ok(v) => v,
+    }
 }
